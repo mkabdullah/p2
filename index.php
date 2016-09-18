d62a4e1f6e267e3c1c581dce9c9af2240dd8d9cb
Muhammad Abdullah <kaleem.abdullah@gmail.com>
2016-09-18 12:35:51 -0400

    work in progress

--- index.php
+++ index.php
@@ -13,13 +13,46 @@
 <body>
 
   <?php
+    // $number_of_words = 5;
+    // $add_number='off';
+    // $add_symbol='off';
+
+    $number_of_words = isset($_POST['number_of_words']) ? $_POST['number_of_words'] : '5';
+    $add_number = isset($_POST['add_number']) ? $_POST['add_number'] : 'off';
+    $add_symbol = isset($_POST['add_symbol']) ? $_POST['add_symbol'] : 'off';
+
+    echo $number_of_words.'<br>';
+    echo $add_number.'<br>';
+    echo $add_symbol.'<br>';
+
+    // if(isset($_POST['number_of_words']))
+    //   echo $_POST["number_of_words"].'<br>';
+    // if(isset($_POST['add_number']))
+    //   echo $_POST['add_number'].'<br>';
+    // if(isset($_POST['add_symbol']))
+    //   echo $_POST['add_symbol'].'<br>';
+
   require ('password_generator.php');
-  generate_xkcd_password();
+
   ?>
 
   <div class="container">
+    <?php generate_xkcd_password($number_of_words); ?>
+    <form method='POST' action='index.php'>
+      <label for='number_of_words'># of Words</label>
+      <input maxlength=1 type='text' name='number_of_words' id='number_of_words'
+        onkeypress='return event.charCode >= 48 && event.charCode <= 57'>
+      <br>
+  		<input type='checkbox' name='add_number' id='add_number' >
+  		<label for='add_number'>Add a number</label>
+  		<br>
+  		<input type='checkbox' name='add_symbol' id='add_symbol' >
+  		<label for='add_symbol'>Add a symbol</label>
+      <br>
+      <input type='submit' value='Generate New Password'>
+    </form>
 
-      <img class="xkcd_img" src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
+    <img class="xkcd_img" src='http://imgs.xkcd.com/comics/password_strength.png' alt='xkcd style passwords'>
 
   </div>
 </body>
--- password_generator.php
+++ password_generator.php
@@ -3,15 +3,27 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-function generate_xkcd_password() {
+function generate_xkcd_password($number_of_words)
+{
+  if(!is_numeric($number_of_words))
+  {
+    echo 'Number of words input should be numeric! <br>';
+    return;
+  }
+
+  if($number_of_words < 1 || $number_of_words > 9)
+  {
+    echo 'Number of words input should be between 1 & 9! <br>';
+    return;
+  }
+
 
   $lines = file('/Users/kaleem/dict/words', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
   $password = '';
-  $max_words = 6;
   $word_count = 0;
 
-  while($word_count < $max_words)
+  while($word_count < $number_of_words)
   {
     $line = strtolower($lines[rand(0, count($lines) - 1)]);
     if(strpos($password, $line) == false)
@@ -21,5 +33,7 @@ function generate_xkcd_password() {
     }
   }
 
-  echo($password.'<br>');
+  echo $password.'<br>';
+
+
 }
