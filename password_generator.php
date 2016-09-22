7d8e3314b48325ca361877deb595bfcc04782e34
Muhammad Abdullah <kaleem.abdullah@gmail.com>
2016-09-21 22:01:48 -0400

    added word length range

--- password_generator.php
+++ password_generator.php
@@ -13,6 +13,8 @@ define('CONST_UPPER_CASE', 'Upper Case');
 define('CONST_LOWER_CASE', 'Lower Case');
 define('CONST_SEPARATOR_HYPHEN', '-');
 define('CONST_SEPARATOR_SPACE', ' ');
+define('MIN_WORD_LENGTH', 4);
+define('MAX_WORD_LENGTH', 10);
 
 /*A function to generate xkcd password. If everthing goes well, it returns an string containing
 the generated password. If there is problems with the input, then it returns an error string
@@ -66,8 +68,11 @@ function generate_xkcd_password($number_of_words, $case, $separator, $include_nu
     $line = trim(strtolower($lines[rand(0, count($lines) - 1)]));
 
     #check if the new word alreay exists in our password
-    #if it does, just ignore it. Also check if it is longer than 3 chars
-    if(strpos($password, $line) === false && strlen($line) > 3)
+    #if it does, just ignore it. Also check if it is within
+    #the proper world length range
+    if(strpos($password, $line) === false
+      && strlen($line) >= MIN_WORD_LENGTH
+      && strlen($line) <= MAX_WORD_LENGTH)
     {
       #increment the word counter
       $word_count++;
